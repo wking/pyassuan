594475c33ec5807bbf64abe9fc57b1e0e56fc5fe
W. Trevor King <wking@tremily.us>
2012-04-18 13:21:15 -0400

    Change my email address from drexel.edu to tremily.us.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 W. Trevor King <wking@drexel.edu>
+# Copyright (C) 2012 W. Trevor King <wking@tremily.us>
 #
 # This file is part of pyassuan.
 #
@@ -28,7 +28,7 @@ _setup(
     name='pyassuan',
     version=__version__,
     maintainer='W. Trevor King',
-    maintainer_email='wking@drexel.edu',
+    maintainer_email='wking@tremily.us',
     url='http://blog.tremily.us/posts/pyassuan/',
     download_url='http://git.tremily.us/?p=pyassuan.git;a=snapshot;h=v{};sf=tgz'.format(__version__),
     license = 'GNU General Public License (GPL)',
