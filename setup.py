7bdffe153e0999625a9e874ce68c5fcf43c3eac8
W. Trevor King <wking@tremily.us>
2018-04-25 14:10:39 -0700

    setup.py: Claim conformance with Python 3.4 through 3.6
    The tests pass on all of these anyway, and I've been running it on
    Python 3.4 for a while now.

--- setup.py
+++ setup.py
@@ -42,6 +42,9 @@ _setup(
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Security :: Cryptography',
         'Topic :: Software Development'
         ],
