1c19d389bbf5894a0bf451e2fc9ddace4317661a
W. Trevor King <wking@tremily.us>
2018-04-25 14:03:32 -0700

    *: Drop nose dependency
    And use Python's unittest discovery instead.  The load_tests link
    between the doctests and unittest is based on [1].
    
    [1]: https://docs.python.org/3.6/library/doctest.html#unittest-api

---
+++ pyassuan/test_error.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2018 W. Trevor King <wking@tremily.us>
+#
+# This file is part of pyassuan.
+#
+# pyassuan is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# pyassuan is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# pyassuan.  If not, see <http://www.gnu.org/licenses/>.
+
+import doctest
+import unittest
+
+from . import error
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(error))
+    return tests
